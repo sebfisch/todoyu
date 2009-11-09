5167428d2b4ae23c4fa9b7c0f40d5ae5df80fa38
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2009-11-09 11:10:30 +0000

    * Project fixes

--- core/lib/js/prototype/Autocompleter.js
+++ core/lib/js/prototype/Autocompleter.js
@@ -1,13 +1,24 @@
 Todoyu.Autocompleter = Class.create(Ajax.Autocompleter, {
-	onComplete: function(request) {
+	onComplete: function(response) {
 			// If a custom onComplete defined
 		if( this.options.onCompleteCustom ) {
-			var funResult = Todoyu.callUserFunction(this.options.onCompleteCustom, window, request);
-				// If the custom function return something, override request
+			var funResult = Todoyu.callUserFunction(this.options.onCompleteCustom, window, response);
+				// If the custom function returns an object, override response
 			if( typeof(funResult) === 'object' ) {
-				request = funResult;
+				response = funResult;
 			}
-		}		
-		this.updateChoices(request.responseText);
+		} else if( response.getTodoyuHeader('acElements') == 0 ) {
+			this.onEmptyResult(response);
+		}
+		
+			// Call default ac handler
+		this.updateChoices(response.responseText);
+	},
+	
+	onEmptyResult: function(response) {
+		new Effect.Highlight(this.element, {
+			'startcolor': '#ff0000',
+			'duration': 2.0
+		});
 	}
 });
