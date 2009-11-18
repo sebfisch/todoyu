1f58e6092afc28e3d2b8c0e50a157c355cd79914
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2009-11-18 08:29:15 +0000

    * Sysmanager cleanup

--- core/TodoyuExtensions.class.php
+++ core/TodoyuExtensions.class.php
@@ -32,15 +32,6 @@ class TodoyuExtensions {
 
 	private static $extKeys = array();
 
-	private static function saveInstalledExtensions() {
-		$file	= PATH_CONFIG . '/extensions.php';
-		$tmpl	= PATH_CORE . '/view/extensions.php.tmpl';
-		$data	= array(
-			'extensions'	=> $GLOBALS['CONFIG']['EXT']['installed']
-		);
-
-		TodoyuFileManager::saveTemplatedFile($file, $tmpl, $data);
-	}
 
 
 
@@ -101,26 +92,6 @@ class TodoyuExtensions {
 	}
 
 
-
-
-	public static function install($extKey) {
-		$GLOBALS['CONFIG']['EXT']['installed'][] = $extKey;
-
-		$GLOBALS['CONFIG']['EXT']['installed'] = array_unique($GLOBALS['CONFIG']['EXT']['installed']);
-
-		self::saveInstalledExtensions();
-	}
-
-
-	public static function uninstall($extKey) {
-		$installed	= array_flip($GLOBALS['CONFIG']['EXT']['installed']);
-
-		unset($installed[$extKey]);
-
-		$GLOBALS['CONFIG']['EXT']['installed']	= array_keys($installed);
-
-		self::saveInstalledExtensions();
-	}
 
 
 
