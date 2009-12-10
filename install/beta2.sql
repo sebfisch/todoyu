643f222324216bb0f31506389a92d42ab33bb22b
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2009-12-10 18:34:19 +0000

    * Added db field: date_track * Implemented rights in bookmarks and calendar

--- install/beta2.sql
+++ install/beta2.sql
@@ -10,3 +10,9 @@ UPDATE `ext_project_task` SET `sorting` = `tasknumber`;
 --
 DROP TABLE `ext_portal_mm_tab_filterset`;
 DROP TABLE `ext_portal_tab`;
+
+
+--
+-- Rename id_user to id_user_create in timetracking
+--
+ALTER TABLE `ext_timetracking_track` CHANGE `id_user` `id_user_create` INT( 10 ) UNSIGNED NOT NULL DEFAULT '0'
