76ac214134d32af7b5c4eb98c2da65c1fec6f8f4
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-02-05 14:17:47 +0000

    * implemented user quickinfo * moved quickinfo class from calendar to core * implemented user info hooks for task details * bugfix: contacttype labels were missing in contact * fixed task details user labels to be rendered via global user label method * bugfix: added forgotten quickinfo type 'birthday' to calendar quickinfo observer uninstall

---
+++ core/TodoyuQuickinfo.class.php
@@ -0,0 +1,118 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 snowflake productions gmbh
+*  All rights reserved
+*
+*  This script is part of the todoyu project.
+*  The todoyu project is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License, version 2,
+*  (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html) as published by
+*  the Free Software Foundation;
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Todoyu Quickinfo
+ *
+ * @package		Todoyu
+ * @subpackage	Core
+ */
+class TodoyuQuickinfo {
+
+	/**
+	 * Infos
+	 *
+	 * @var	Array
+	 */
+	private $data = array();
+
+	/**
+	 * Create a new quickinfo object
+	 *
+	 */
+	public function __construct() {
+
+	}
+
+
+	/**
+	 * Add new info
+	 *
+	 * @param	String		$key		Identifier and classname
+	 * @param	String		$label		Labeltext
+	 * @param	Integer		$position
+	 */
+	public function addInfo($key, $label, $position = 100) {
+		$this->data[$key] = array(
+			'key'		=> $key,
+			'label'		=> $label,
+			'position'	=> intval($position)
+		);
+	}
+
+
+
+	/**
+	 * Remove an info by key
+	 *
+	 * @param	String		$key
+	 */
+	public function removeInfo($key) {
+		unset($this->data[$key]);
+	}
+
+
+
+	/**
+	 * Get current infos
+	 *
+	 * @return	Array
+	 */
+	public function getInfos() {
+		return TodoyuArray::sortByLabel($this->data, 'position');
+	}
+
+
+
+	/**
+	 * Get infos encoded as json
+	 *
+	 * @return	String
+	 */
+	public function getInfoJSON() {
+		return json_encode($this->getInfos());
+	}
+
+
+
+	/**
+	 * Print info struct json encoded
+	 *
+	 */
+	public function printInfoJSON() {
+		TodoyuHeader::sendHeaderJSON();
+
+		echo $this->getInfoJSON();
+	}
+
+
+
+	/**
+	 * Remove all infos
+	 *
+	 */
+	public function clear() {
+		$this->data = array();
+	}
+
+}
+
+?>
