6dd65752d70a81a8b280c6c4b5a02b6924397ab3
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-03-18 13:23:59 +0000

    * added test class for html filter

---
+++ core/tests/TodoyuHtmlFilterTest.class.php
@@ -0,0 +1,143 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 snowflake productions gmbh
+*  All rights reserved
+*
+*  This script is part of the todoyu project.
+*  The todoyu project is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License, version 2,
+*  (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html) as published by
+*  the Free Software Foundation;
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Test for: TodoyuHtmlFilter
+ *
+ * @package		Todoyu
+ * @subpackage	Core
+ */
+class TodoyuHtmlFilterTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @var Array
+	 */
+	private $array;
+
+
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 */
+	protected function setUp() {
+		$this->array = array(
+
+		);
+
+	}
+
+
+
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 */
+	protected function tearDown() {
+
+	}
+
+
+
+	/**
+	 * Test TodoyuHtmlFilter::getBadTags
+	 *
+	 * @todo Implement testGetBadTags().
+	 */
+	public function testGetBadTags() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+		  'This test has not been implemented yet.'
+		);
+	}
+
+
+
+	/**
+	 * Test TodoyuHtmlFilter::clean
+	 * @todo Implement testClean().
+	 */
+	public function testClean() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+		  'This test has not been implemented yet.'
+		);
+	}
+
+
+
+
+
+	/**
+	 * Test TodoyuHtmlFilter::entitySafeLimitWordsLen($string, $maxLen = 45)
+	 * @todo Implement testEntitySafeLimitWordsLen().
+	 */
+	public function testEntitySafeLimitWordsLen() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+		  'This test has not been implemented yet.'
+		);
+	}
+
+
+
+
+
+	/**
+	 * Test TodoyuHtmlFilter::htmlSafeChunkSplit($html, $size, $delim)
+	 * @todo Implement testHtmlSafeChunkSplit().
+	 */
+	public function testHtmlSafeChunkSplit() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+		  'This test has not bhtmlSafeChunkSplitemented yet.'
+		);
+	}
+
+
+
+	/**
+	 * Test TodoyuHtmlFilter::escapeBadTags(array $match)
+	 * @todo Implement testEscapeBadTags().
+	 */
+	public function testEscapeBadTags() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+		  'This escapeBadTags not bhtmlSafeChunkSplitemented yet.'
+		);
+	}
+
+
+
+	/**
+	 * Test TodoyuHtmlFilter::escapeBadTag(array $match)
+	 * @todo Implement testEscapeBadTag().
+	 */
+	public function testEscapeBadTag() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+		  'This escapeBadTags not bhtmlSafeChunkSplitemented yet.'
+		);
+	}
+
+}
+
+?>
