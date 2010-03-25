0d6e24e0946e647498b9db59d6b82cd6ca5be0f2
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-03-25 14:56:54 +0000

    * Switch license text from GPL to BSD in all files

--- config/override.php
+++ config/override.php
@@ -1,4 +1,22 @@
 <?php
+/****************************************************************************
+* todoyu is published under the BSD License:
+* http://www.opensource.org/licenses/bsd-license.php
+*
+* Copyright (c) 2010, snowflake productions gmbh
+* All rights reserved.
+*
+* This script is part of the todoyu project.
+* The todoyu project is free software; you can redistribute it and/or modify
+* it under the terms of the BSC License.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD License
+* for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script.
+*****************************************************************************/
 
 	// This config file is loaded when everything is loaded and before the request is handled
 	// Here you can override any extension configuration with your own setting
