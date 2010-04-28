ec07620240e7caef2e16b08af7135089d2bce7d3
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-04-28 10:02:43 +0000

    * Updated snowflake name in all files (GmbH)

--- config/extconf.php
+++ config/extconf.php
@@ -3,7 +3,7 @@
 * todoyu is published under the BSD License:
 * http://www.opensource.org/licenses/bsd-license.php
 *
-* Copyright (c) 2010, snowflake productions gmbh
+* Copyright (c) 2010, snowflake productions GmbH, Switzerland
 * All rights reserved.
 *
 * This script is part of the todoyu project.
@@ -27,12 +27,12 @@
 
 	// Extension config
 Todoyu::$CONFIG['EXT']['admin']['extConf'] = unserialize(stripslashes('a:0:{}'));
-Todoyu::$CONFIG['EXT']['project']['extConf'] = unserialize(stripslashes('a:5:{s:6:\"status\";s:1:\"1\";s:16:\"id_role_assigned\";s:1:\"1\";s:13:\"id_role_owner\";s:1:\"1\";s:11:\"id_worktype\";s:2:\"35\";s:18:\"estimated_workload\";i:0;}'));
+Todoyu::$CONFIG['EXT']['project']['extConf'] = unserialize(stripslashes('a:0:{}'));
 Todoyu::$CONFIG['EXT']['sysmanager']['extConf'] = unserialize(stripslashes('a:0:{}'));
 Todoyu::$CONFIG['EXT']['loginpage']['extConf'] = unserialize(stripslashes('a:0:{}'));
 Todoyu::$CONFIG['EXT']['portal']['extConf'] = unserialize(stripslashes('a:0:{}'));
 Todoyu::$CONFIG['EXT']['calendar']['extConf'] = unserialize(stripslashes('a:0:{}'));
-Todoyu::$CONFIG['EXT']['timetracking']['extConf'] = unserialize(stripslashes('a:1:{s:12:\"stopOnLogout\";i:0;}'));
+Todoyu::$CONFIG['EXT']['timetracking']['extConf'] = unserialize(stripslashes('a:0:{}'));
 Todoyu::$CONFIG['EXT']['daytracks']['extConf'] = unserialize(stripslashes('a:0:{}'));
 Todoyu::$CONFIG['EXT']['search']['extConf'] = unserialize(stripslashes('a:0:{}'));
 Todoyu::$CONFIG['EXT']['bookmark']['extConf'] = unserialize(stripslashes('a:0:{}'));
@@ -41,6 +41,6 @@ Todoyu::$CONFIG['EXT']['comment']['extConf'] = unserialize(stripslashes('a:0:{}'
 Todoyu::$CONFIG['EXT']['profile']['extConf'] = unserialize(stripslashes('a:0:{}'));
 Todoyu::$CONFIG['EXT']['assets']['extConf'] = unserialize(stripslashes('a:0:{}'));
 Todoyu::$CONFIG['EXT']['demologin']['extConf'] = unserialize(stripslashes('a:0:{}'));
-Todoyu::$CONFIG['EXT']['bugtracker']['extConf'] = unserialize(stripslashes('a:3:{s:6:\"enable\";i:0;s:6:\"person\";s:1:\"6\";s:7:\"project\";s:1:\"4\";}'));
+Todoyu::$CONFIG['EXT']['bugtracker']['extConf'] = unserialize(stripslashes('a:0:{}'));
 
 ?>
