d261939784b476394703290e69a910728e2fe09d
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-05-26 13:10:45 +0000

    * Unittesting fixes for hudson

--- core/tests/TodoyuArrayTest.class.php
+++ core/tests/TodoyuArrayTest.class.php
@@ -18,6 +18,9 @@
 * This copyright notice MUST APPEAR in all copies of the script.
 *****************************************************************************/
 
+
+require_once( PATH_CORE . '/TodoyuArray.class.php');
+
 /**
  * Test for: TodoyuArray
  *
