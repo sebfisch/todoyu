06ce284bbfbad761ea6fb6e367a6e4b8b6303caf
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-05-26 14:03:29 +0000

    * Setup script

--- core/ci/setup.php
+++ core/ci/setup.php
@@ -1,15 +1,10 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: ferni
- * Date: 26.05.2010
- * Time: 15:26:26
- * To change this template use File | Settings | File Templates.
- */
 
-touch('/var/lib/hudson/jobs/todoyu/fun.txt');
+require_once(realpath(dirname(__FILE__) . '/../inc/global.php'));
+require_once(PATH_CORE . '/inc/init.php');
 
-echo "TEST";
+TodoyuExtInstaller::installExtension('unittest');
 
+echo "Unittest wurde installiert";
 
 ?>
