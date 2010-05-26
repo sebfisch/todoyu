7e0aacf21f9a65cb29348ca15e2f4d928d997648
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-05-26 13:33:04 +0000

    * Added ci setup file

---
+++ core/ci/setup.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: ferni
+ * Date: 26.05.2010
+ * Time: 15:26:26
+ * To change this template use File | Settings | File Templates.
+ */
+
+touch('/var/lib/hudson/jobs/todoyu/fun.txt');
+
+echo "TEST";
+
+
+?>
