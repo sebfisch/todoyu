dd3fde16a994cea601baee6d678b6932bede072f
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-06-03 14:46:23 +0000

    * Assets fixes

--- core/config/assets.php
+++ core/config/assets.php
@@ -102,16 +102,16 @@ Todoyu::$CONFIG['FE']['PAGE']['assets'] = array(
 			'position'	=> 23,
 			'merge'		=> true,
 			'localize'	=> false,
-			'compress'	=> true
+			'compress'	=> false
 		),
 		array(
-//			'file'		=> 'lib/js/highcharts/js/highcharts.src.js',
-			'file'		=> 'lib/js/highcharts/js/highcharts.js',
+			'file'		=> 'lib/js/highcharts/js/highcharts.src.js',
+//			'file'		=> 'lib/js/highcharts/js/highcharts.js',
 			'position'	=> 23,
 			'merge'		=> true,
 			'localize'	=> false,
-			'compress'	=> true
-		),
+			'compress'	=> false
+		),		
 
 /**
  * Note: JSCalendar lang file is added at end of initialization
