ee8d3cc7a796eb8a92ed83835b306dd00110028c
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-07-13 07:06:16 +0000

    * removed premium extensions config from extconf

--- config/extconf.php
+++ config/extconf.php
@@ -27,7 +27,7 @@
 
 	// Extension config
 Todoyu::$CONFIG['EXT']['admin']['extConf'] = unserialize(stripslashes('a:0:{}'));
-Todoyu::$CONFIG['EXT']['project']['extConf'] = unserialize(stripslashes('a:11:{s:5:\"title\";s:0:\"\";s:11:\"description\";s:0:\"\";s:10:\"date_start\";s:1:\"0\";s:8:\"date_end\";s:1:\"0\";s:13:\"date_deadline\";s:1:\"0\";s:6:\"status\";s:1:\"0\";s:11:\"id_worktype\";s:1:\"3\";s:18:\"estimated_workload\";i:0;s:9:\"is_public\";i:0;s:20:\"person_assigned_role\";s:1:\"0\";s:17:\"person_owner_role\";s:1:\"0\";}'));
+Todoyu::$CONFIG['EXT']['project']['extConf'] = unserialize(stripslashes('a:0:{}'));
 Todoyu::$CONFIG['EXT']['sysmanager']['extConf'] = unserialize(stripslashes('a:0:{}'));
 Todoyu::$CONFIG['EXT']['loginpage']['extConf'] = unserialize(stripslashes('a:0:{}'));
 Todoyu::$CONFIG['EXT']['portal']['extConf'] = unserialize(stripslashes('a:0:{}'));
@@ -40,9 +40,5 @@ Todoyu::$CONFIG['EXT']['contact']['extConf'] = unserialize(stripslashes('a:0:{}'
 Todoyu::$CONFIG['EXT']['comment']['extConf'] = unserialize(stripslashes('a:0:{}'));
 Todoyu::$CONFIG['EXT']['profile']['extConf'] = unserialize(stripslashes('a:0:{}'));
 Todoyu::$CONFIG['EXT']['assets']['extConf'] = unserialize(stripslashes('a:0:{}'));
-Todoyu::$CONFIG['EXT']['currency']['extConf'] = unserialize(stripslashes('a:0:{}'));
-Todoyu::$CONFIG['EXT']['projectbilling']['extConf'] = unserialize(stripslashes('a:0:{}'));
-Todoyu::$CONFIG['EXT']['unittest']['extConf'] = unserialize(stripslashes('a:0:{}'));
-Todoyu::$CONFIG['EXT']['demologin']['extConf'] = unserialize(stripslashes('a:0:{}'));
 
 ?>
