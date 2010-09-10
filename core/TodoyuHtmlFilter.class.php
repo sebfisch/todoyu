9e063a33ea3abd81b76f011df3b49be5b13d6f25
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-09-10 13:17:50 +0000

    * improved URL parser to also detect URLs with post-params and email-addresses

--- core/TodoyuHtmlFilter.class.php
+++ core/TodoyuHtmlFilter.class.php
@@ -157,11 +157,11 @@ class TodoyuHtmlFilter {
 	 * @return	String
 	 */
 	public static function substituteUrlsByHperlinks($text)	{
-			// Match protocol://address/path/
-		$text = ereg_replace("[a-zA-Z]+://([.]?[a-zA-Z0-9_/-])*", "<a href=\"\\0\" target=\"_blank\">\\0</a>", $text);
+		$text = eregi_replace('(((f|ht){1}tp://)[-a-zA-Z0-9@:%_\+.~#?&//=]+)', '<a href="\\1"  target=\"_blank\">\\1</a>', $text);
 
-			// Match www.something
-		$text = ereg_replace("(^| )(www([.]?[a-zA-Z0-9_/-])*)", "\\1<a href=\"http://\\2\"  target=\"_blank\">\\2</a>", $text);
+		$text = eregi_replace('([[:space:]()[{}])(www.[-a-zA-Z0-9@:%_\+.~#?&//=]+)', '\\1<a href="http://\\2" target="_blank">\\2</a>', $text);
+
+		$text = eregi_replace('([_\.0-9a-z-]+@([0-9a-z][0-9a-z-]+\.)+[a-z]{2,3})', '<a href="mailto:\\1">\\1</a>', $text);
 
 		return $text;
 	}
