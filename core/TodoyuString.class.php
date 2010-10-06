b4cb8cf4c92ce97d2587b2ae45baffd2e20b8065
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-10-06 08:17:21 +0000

    * Add square brackeds ([]) to allowed link characters #61

--- core/TodoyuString.class.php
+++ core/TodoyuString.class.php
@@ -509,11 +509,11 @@ class TodoyuString {
 	 */
 	public static function replaceUrlWithLink($htmlContent) {
 				// Find full links with prefixed protocol
-		$patternFull	= '/(^|[^"])((?:http|https|ftp|ftps):\/\/[-\w@:%+.~#?&;\/=]+)/';
+		$patternFull	= '/(^|[^"])((?:http|https|ftp|ftps):\/\/[-\w@:%+.~#?&;\/=\[\]]+)/';
 		$replaceFull	= '\1<a href="\2"  target="_blank">\2</a>';
 
 			// Find links which are not prefixed with a protocol, use http
-		$patternSimple	= '/(^|[> ])((?:[\w\.-]+)\.(?:[\w-]{2,})\.(?:[a-zA-Z-]{2,6})[-\w@:%+.~#?&;\/=]*)/';
+		$patternSimple	= '/(^|[> ])((?:[\w\.-]+)\.(?:[\w-]{2,})\.(?:[a-zA-Z-]{2,6})[-\w@:%+.~#?&;\/=\[\]]*)/';
 		$replaceSimple	= '\1<a href="http://\2" target="_blank">\2</a>';
 
 			// Find mailto links
