683f21de7b533f4c29780cbfac2bb64e51e18742
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2011-03-21 15:09:28 +0000

    * Changed trunk version to 2.1.0b1

--- core/inc/version.php
+++ core/inc/version.php
@@ -22,7 +22,7 @@
  * Todoyu version
  *
  */
-define('TODOYU_VERSION', '2.1.0a');
-define('TODOYU_UPDATE', '2011-02-21');
+define('TODOYU_VERSION', '2.1.0b1');
+define('TODOYU_UPDATE', '2011-03-25');
 
 ?>
--- install/update/db/2.1.0b1.sql
+++ install/update/db/2.1.0b1.sql
@@ -3,13 +3,12 @@ RENAME TABLE `ext_comment_feedback` TO `ext_comment_mm_comment_feedback` ;
 RENAME TABLE `system_errorlog` TO `system_log_error` ;
 
 -- Remove tables
-DROP TABLE `system_log` ;
+DROP TABLE `system_log`;
 
 -- Rename worktype to activity --
 RENAME TABLE `ext_project_worktype` TO `ext_project_activity` ;
 ALTER TABLE `ext_project_task` CHANGE `id_worktype` `id_activity` SMALLINT( 6 ) NOT NULL DEFAULT '0';
 ALTER TABLE `ext_projectbilling_rate` CHANGE `id_worktype` `id_activity` SMALLINT( 6 ) UNSIGNED NOT NULL;
-ALTER TABLE `ext_project_taskpreset` CHANGE `id_worktype` `id_activity` SMALLINT( 6 ) NOT NULL DEFAULT '0';
 
 -- container status --- (open)
 UPDATE `ext_project_task` SET status = 2 WHERE `type` = 2;
