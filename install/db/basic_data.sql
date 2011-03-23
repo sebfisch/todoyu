bcaac39133e7fb93b52a0b4af19c41de4ba8a21f
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2011-03-23 09:50:17 +0000

    * Bugfixes

--- install/db/basic_data.sql
+++ install/db/basic_data.sql
@@ -2,7 +2,7 @@
 -- Data for Table `ext_contact_person`
 --
 
-INSERT INTO `ext_contact_person` (`id`, `date_create`, `date_update`, `id_person_create`, `deleted`, `username`, `password`, `email`, `is_admin`, `active`, `firstname`, `lastname`, `shortname`, `salutation`, `title`, `birthday`) VALUES
+INSERT INTO `ext_contact_person` (`id`, `date_create`, `date_update`, `id_person_create`, `deleted`, `username`, `password`, `email`, `is_admin`, `is_active`, `firstname`, `lastname`, `shortname`, `salutation`, `title`, `birthday`) VALUES
 (1, 1246615200, 1264780312, 0, 0, 'admin', '5f4dcc3b5aa765d61d8327deb882cf99', 'demo@todoyu.com', 1, 1, 'Adam', 'Admin', 'ALAD', 'm', '', '0000-00-00');
 
 -- --------------------------------------------------------
