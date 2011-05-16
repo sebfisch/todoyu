190c9553856f2bf4e840ec67d7bcf7ffcf3725f1
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2011-05-16 11:51:41 +0000

    * adapted timespan formatting of list-item of single-day events globally (comma between date and time)

--- core/model/TodoyuTime.class.php
+++ core/model/TodoyuTime.class.php
@@ -382,8 +382,8 @@ class TodoyuTime {
 	 */
 	public static function formatTimespan($dateStart, $dateEnd, $withDuration = false) {
 			// Start and end at same day
-		if( self::getStartOfDay($dateStart) == self::getStartOfDay($dateEnd) ) {
-			$formatted	= self::format($dateStart, 'datetime') . ' - ' . self::format($dateEnd, 'time');
+		if( self::getStartOfDay($dateStart) === self::getStartOfDay($dateEnd) ) {
+			$formatted	= self::format($dateStart, 'date') . ', ' . self::format($dateStart, 'time') . ' - ' . self::format($dateEnd, 'time');
 		} else {
 				// Different days
 			$formatted	= self::format($dateStart, 'datetime') . ' - ' . self::format($dateEnd, 'datetime');
