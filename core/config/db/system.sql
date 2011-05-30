ebb7c31162206629d2fa7791278efdeb032185b8
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2011-05-30 08:05:39 +0000

    * changed: DB field system_token.token from varchar(255) to int(11)

--- core/config/db/system.sql
+++ core/config/db/system.sql
@@ -92,12 +92,12 @@ CREATE TABLE `system_lock` (
 --
 
 CREATE TABLE IF NOT EXISTS `system_scheduler` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date_execute` int(11) NOT NULL,
-  `class` varchar(100) NOT NULL,
-  `is_success` tinyint(1) NOT NULL,
-  `message` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`date_execute` int(11) NOT NULL,
+	`class` varchar(100) NOT NULL,
+	`is_success` tinyint(1) NOT NULL,
+	`message` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -107,15 +107,15 @@ CREATE TABLE IF NOT EXISTS `system_scheduler` (
 --
 
 CREATE TABLE IF NOT EXISTS `system_token` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date_create` int(11) NOT NULL,
-  `date_update` int(11) NOT NULL,
-  `id_person_create` int(11) NOT NULL,
-  `id_person_owner` int(11) NOT NULL,
-  `deleted` tinyint(1) NOT NULL DEFAULT '0',
-  `ext` smallint(6) NOT NULL,
-  `token_type` varchar(255) NOT NULL,
-  `hash` varchar(32) NOT NULL,
-  `callback_params` text NOT NULL,
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`date_create` int(11) NOT NULL,
+	`date_update` int(11) NOT NULL,
+	`id_person_create` int(11) NOT NULL,
+	`id_person_owner` int(11) NOT NULL,
+	`deleted` tinyint(1) NOT NULL DEFAULT '0',
+	`ext` smallint(6) NOT NULL,
+	`token_type` int(11) NOT NULL,
+	`hash` varchar(32) NOT NULL,
+	`callback_params` text NOT NULL,
   PRIMARY KEY (`id`),
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
