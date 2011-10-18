702fc3f708113b98fec5acfb7f7b522f08cf279b
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2011-10-18 09:22:02 +0000

    * Changed version todoyu 2.2.0dev

--- core/inc/version.php
+++ core/inc/version.php
@@ -22,7 +22,7 @@
  * Todoyu version
  *
  */
-define('TODOYU_VERSION', '2.2.0a');
-define('TODOYU_UPDATE', '2011-09-30');
+define('TODOYU_VERSION', '2.2.0dev');
+define('TODOYU_UPDATE', '2011-10-18');
 
 ?>
