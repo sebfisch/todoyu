677d530b37532bd807f2ee9bc09a6127aa592063
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2012-07-10 16:42:53 +0000

    * Hide status bar with path

--- core/asset/js/Ui.js
+++ core/asset/js/Ui.js
@@ -83,7 +83,8 @@ Todoyu.Ui = {
 				theme_advanced_toolbar_location:	'bottom',
 				theme_advanced_buttons1:	'bold,italic,strikethrough,|,bullist,numlist,outdent,indent,|,link,unlink,|,hr',
 				theme_advanced_buttons2: 	'',
-				theme_advanced_buttons3:	''
+				theme_advanced_buttons3:	'',
+				theme_advanced_statusbar_location: 'none'
 			};
 		}
 
